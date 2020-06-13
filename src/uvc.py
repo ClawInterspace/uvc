b3cfa830d8b7f669db8dfc14ce04cc980c47ee76
ClawInterspace <alanliu71104@gmail.com>
2020-06-13 13:03:55 +0800

    Batch update done

--- src/uvc.py
+++ src/uvc.py
@@ -2,7 +2,9 @@
 import argparse
 import os
 
+from custom_actions.update_branch import update_branch
 from git.cmd_firer import get_all_git_folders, fire_commands
+from models import config
 from utils import logging_helper
 
 logger = logging_helper.get_logger()
@@ -13,22 +15,21 @@ arg_parser = argparse.ArgumentParser(description='parse argument')
 arg_parser.add_argument('-r', '--root', default=os.getcwd())
 arg_parser.add_argument('-c', '--command')
 arg_parser.add_argument('-g', '--git')
+arg_parser.add_argument('-u', '--update')
 args = arg_parser.parse_args()
 
-git_folders = get_all_git_folders(args.root)
+config.git_folders = get_all_git_folders(args.root)
 # for d in git_folders:
 #     print(d)
 if args.command:
-    command = args.command
+    config.command = args.command
 elif args.git:
-    command = f'git {args.git}'
+    config.command = f'git {args.git}'
 
-console_border_width = 80
+console_border_width = config.console_border_width
 
-for wd, out in fire_commands(git_folders, command):
-    print(f" {'':_<{console_border_width}} ")
-    print(f'|{wd:^{console_border_width}}|')
-    out_msg = f'=> {command}'
-    print(f'|{out_msg:^{console_border_width}}|')
-    print(f"|{'':_<{console_border_width}}|")
-    print(out)
+update_branch('develop')
+
+# for _ in fire_commands(config.git_folders, config.command): pass
+# deque(fire_commands(config.git_folders, config.command), maxlen=0)
+_ = list(fire_commands(config.git_folders, config.command))
