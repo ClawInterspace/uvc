73358a87f2d60196f6ab80bcd7182cf85a8382f5
ClawInterspace <alanliu71104@gmail.com>
2020-09-09 23:45:23 +0800

    add update command

--- src/uvc.py
+++ src/uvc.py
@@ -14,23 +14,16 @@ logger.info('app start!')
 arg_parser = argparse.ArgumentParser(description='parse argument')
 arg_parser.add_argument('-r', '--root', default=os.getcwd())
 arg_parser.add_argument('-c', '--command')
+arg_parser.add_argument('-u', '--update')
 args = arg_parser.parse_args()
 
 config.git_folders = get_all_git_folders(args.root)
 console_border_width = config.console_border_width
 
-# TODO: change to Click
-# TODO: setup the parameter priority: command line > config file > default
-# TODO: refactor config loading location
-# TODO: build up unittest for get git folder
-# TODO: make a setup script
-
-
 if args.command:
     config.command = args.command
+    _ = list(fire_commands(config.git_folders, config.command))
 
-update_branch('develop')
-
-# for _ in fire_commands(config.git_folders, config.command): pass
-# deque(fire_commands(config.git_folders, config.command), maxlen=0)
-_ = list(fire_commands(config.git_folders, config.command))
+if args.update:
+    branch_name = args.update
+    update_branch(branch_name)
