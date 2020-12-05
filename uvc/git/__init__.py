860c27b24562e5910e3554a83938ae39132e049a
MSI\s8711 <alanliu71104@gmail.com>
2020-12-05 21:42:37 +0800

    enhance fire command

--- uvc/git/__init__.py
+++ uvc/git/__init__.py
@@ -2,22 +2,11 @@ import click
 
 from uvc.git.custom_actions.update_branch import update_branch
 from uvc.git.utils import get_repo_folders
-from uvc.models import config
-from uvc.utils.command import fire_commands
 
 
 @click.group()
 def git():
     pass
-
-
-@git.command(context_settings=dict(ignore_unknown_options=True,))
-@click.argument('gitcmd', nargs=-1, type=click.UNPROCESSED)
-def fire(gitcmd):
-    """Issue a command to all of repos.
-    """
-    cmdline = ' '.join(gitcmd)
-    _ = list(fire_commands(config.git_folders, f'git {cmdline}'))
 
 
 @git.command()
--- uvc/main.py
+++ uvc/main.py
@@ -3,6 +3,7 @@ import os
 
 import click
 
+from uvc.utils.command import fire_commands
 from uvc.models import config
 from uvc.utils import logging
 from uvc.git import git, get_repo_folders
@@ -18,7 +19,17 @@ def cli(root):
     config.git_folders = get_repo_folders(root)
 
 
+@git.command(context_settings=dict(ignore_unknown_options=True,))
+@click.argument('cmds', nargs=-1, type=click.UNPROCESSED)
+def fire(cmds):
+    """Issue a command to all of repos.
+    """
+    cmdline = ' '.join(cmds)
+    _ = list(fire_commands(config.git_folders, f'{cmdline}'))
+
+
 cli.add_command(git)
+cli.add_command(fire)
 
 if __name__ == '__main__':
     cli()
