ccf7347017a2d32dc647a056eb40e453498de9f0
ClawInterspace <alanliu71104@gmail.com>
2021-01-08 16:47:55 +0800

    make command trace output as an option for cli

--- uvc/models/config.py
+++ uvc/models/config.py
@@ -2,6 +2,7 @@ import platform
 
 root_dir = ''
 git_folders = []
+trace_command = True
 command = ''
 console_border_width = 140
 
--- uvc/utils/command.py
+++ uvc/utils/command.py
@@ -7,51 +7,60 @@ from uvc.utils.logging import get_logger
 logger = get_logger()
 
 
-def fire_commands(dest_wds: list, command: object, show_output: bool = True) -> tuple:
+def fire_commands(dest_wds: list, command: object) -> tuple:
     """
 
     :param dest_wds: destination working dictionary
     :param command: the target command
-    :param show_output: set False to cancel display
     :return: the corresponding working folder's output
     """
+    trace_cmd = config.trace_command
     src_dir = os.getcwd()
     for working_dir in dest_wds:
         try:
             target_dir, output = _issue_command_in_directory(command, src_dir, working_dir)
-            if show_output:
+            if trace_cmd:
                 display(target_dir, command, output)
             yield target_dir, output
         except Exception as e:
-            print(e)
+            if trace_cmd:
+                print(e)
             continue
 
 
-def fire_commands_respectively(wd_and_cmd_maps: dict, show_output: bool = True) -> tuple:
+def fire_commands_respectively(wd_and_cmd_maps: dict) -> tuple:
     """
 
-    :param show_output: flag for show output or not
     :param wd_and_cmd_maps: a dictionary for command and target dir maps
     :return:
     """
+    trace_cmd = config.trace_command
     src_dir = os.getcwd()
     for working_dir, issue_cmd in wd_and_cmd_maps.items():
         target_dir, output = _issue_command_in_directory(issue_cmd, src_dir, working_dir)
-        if show_output:
+        if trace_cmd:
             display(target_dir, issue_cmd, output)
         yield target_dir, output
 
 
-def _issue_command_in_directory(issue_cmd, origin_dir, target_dir):
+def _issue_command_in_directory(issue_cmd, origin_dir, target_dir) -> tuple:
+    """
 
+    :param issue_cmd: the command you want to issue
+    :param origin_dir: the path you want to return, usually as cwd
+    :param target_dir: the working dir you used
+    :return:
+    """
+    trace_cmd = config.trace_command
     try:
         os.chdir(target_dir)
         result = subprocess.run(issue_cmd, stdout=subprocess.PIPE, shell=config.is_shell)
         output = result.stdout.decode('utf-8')
         return target_dir, output
     except Exception as e:
-        print(Exception(f': Fire "{issue_cmd}" failed'))
-        raise
+        if trace_cmd:
+            print(Exception(f': Fire "{issue_cmd}" failed'))
+            raise
     finally:
         os.chdir(origin_dir)
 
@@ -59,8 +68,8 @@ def _issue_command_in_directory(issue_cmd, origin_dir, target_dir):
 def display(wd, cmd, output):
     console_border_width = config.console_border_width
     print(f" {'':_<{console_border_width}} ")
-    print(f'|{wd:^{console_border_width}}|')
+    print(f'|{wd:<{console_border_width}}|')
     out_msg = f'=> {cmd}'
-    print(f'|{out_msg:^{console_border_width}}|')
+    print(f'|{out_msg:<{console_border_width}}|')
     print(f"|{'':_<{console_border_width}}|")
     print(output)
