ff1057908bde8c8288b78c4ae73ff330af7a26ea
ClawInterspace <alanliu71104@gmail.com>
2021-03-08 08:47:06 +0800

    add checkout action

---
+++ uvc/git/check.py
@@ -0,0 +1,9 @@
+def is_stashed_message_equal(repo_path: str, stash_index: int, check_msg: str) -> bool:
+    """
+
+    :param repo_path: the target check repo path
+    :param check_msg: verified message
+    :return: boolean
+    """
+
+    pass
